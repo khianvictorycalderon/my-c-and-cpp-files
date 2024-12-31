601165859f0e7870228e206f0366861794d3af64
Khian Calderon <ckhianvictory@gmail.com>
2024-12-31 12:30:29 +0800

    Added C++ Comment

---
+++ C++_Comment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	// This is a one line comment
+	
+	/*
+		This is a multi-line comment
+	*/
+	std::cout << "Comment are not visible";
+	
+	std::cin.get(); // Pause the program until enter was clicked
+	return 0;
+}
